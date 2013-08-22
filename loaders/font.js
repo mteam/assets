55455f9acc1fd5aadb82ea9971957bc7e1fc7db4
Michal Miskernik <michal.miskernik@gmail.com>
2013-08-22 18:35:44 +0200

    make font loading work in ios safari

--- loaders/font.js
+++ loaders/font.js
@@ -1,15 +1,10 @@
-var URL = window.URL || window.webkitURL;
-
 module.exports = function(name, url) {
   var xhr = new XMLHttpRequest();
   xhr.open('GET', url, true);
-  xhr.responseType = 'blob';
 
   return function(done) {
     xhr.onload = function() {
-      var url = URL.createObjectURL(xhr.response);
       addFont(name, url);
-
       done();
     };
 
